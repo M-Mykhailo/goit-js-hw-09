5d1d224c7e4435269324fad36ca4758b22fb270b
M-Mykhailo <119888711+M-Mykhailo@users.noreply.github.com>
2023-05-11 14:58:32 +0000

    Deploying to gh-pages from @ M-Mykhailo/goit-js-hw-09@2135a53d50e387a3d4a5ab8089db398d1d7b2db2 🚀

--- 01-color-switcher.f17af5db.js
+++
@@ -1,2 +0,0 @@
-const t={btnStart:document.querySelector("[data-start]"),btnStop:document.querySelector("[data-stop]"),body:document.querySelector("body")};let e=null;t.btnStart.addEventListener("click",(function(){e=setInterval((()=>{const e=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`;t.body.style.backgroundColor=e}),1e3),t.btnStart.disabled=!0,t.btnStart.disabled=!1})),t.btnStop.addEventListener("click",(function(){clearInterval(e),t.btnStart.disabled=!1,t.btnStop.disabled=!0}));
-//# sourceMappingURL=01-color-switcher.f17af5db.js.map
